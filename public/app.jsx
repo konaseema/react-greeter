71ef8d28c1b10c2404505d6a508f81939ea09976
Vasisht A. Shastry <shastryucsb@gmail.com>
2017-05-02 21:17:36 -0500

    fix default prop issues and add webpack

---
+++ public/app.jsx
@@ -0,0 +1,10 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var Greeter = require('Greeter');
+
+var firstName = 'BIG V DAWG'
+
+ReactDOM.render(
+    <Greeter/>,
+    document.getElementById('app')
+); 
