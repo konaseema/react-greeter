fa41b2e25ae0a6d5c4028bcc3ce8789ec51090d8
Vasisht A. Shastry <shastryucsb@gmail.com>
2017-05-02 21:50:57 -0500

    refix of default prop in app file

--- public/app.jsx
+++ public/app.jsx
@@ -2,8 +2,6 @@ var React = require('react');
 var ReactDOM = require('react-dom');
 var Greeter = require('Greeter');
 
-var firstName = 'BIG V DAWG'
-
 ReactDOM.render(
     <Greeter/>,
     document.getElementById('app')
